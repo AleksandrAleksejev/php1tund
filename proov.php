8b59ac97ef93988bfb10135c08cc164b77a9f0b4
Opilane TTHK <opilane@tthk.ee>
2024-11-04 10:21:11 +0200

    Initial commit

---
+++ proov.php
@@ -0,0 +1,55 @@
+<?php
+echo "Tere hommikust!";
+echo "<br>";
+$muutaja="PHP on skriptikeel";
+echo "<strong>";
+echo $muutaja;
+echo "</strong>";
+echo "<br>";
+//Tekstifunktsioonid
+echo "<h2>Tekstifunktsioonid<h2>";
+$tekst="Esmaspäev on 4.november";
+echo $tekst;
+//kõik tähed on suured
+echo "<br>";
+echo strtoupper($tekst); // ei tunne ä täht
+echo "<br>";
+echo mb_strtoupper($tekst); // tunneb ä
+//kõik tähed on väiksed
+echo "<br>";
+echo strtolower($tekst);
+//iga sõna algab suure tähega
+echo "<br>";
+echo ucwords($tekst);
+echo "<br>";
+//teksti pikkus
+echo "Teksti pikkus - " .strlen($tekst);
+echo "<br>";
+//eraldame esimesed 5 tähte
+echo "Esimesed 5 tähte ".substr($tekst,0,5);
+echo "<br>";
+//leiame on positsiooni
+$otsing="on";
+echo "On asukoht lauses on " .strpos($tekst,$otsing);
+echo "<br>";
+// eralda esimene sõna kuni $otsing
+echo "Esimese sõna"  .substr($tekst, 0,strpos($tekst,$otsing));
+echo "<br>";
+//eralda peale esimesest sõna, alates "on"
+echo substr($tekst,strpos($tekst,$otsing));
+echo "<br>";
+echo "<h2>Kasutame veebis kasutavaid näidised</h2>";
+echo "<br>";
+echo "sõnade arv laused - ".str_word_count($tekst);//sõnade arv laused
+//Iseseisvalt - teksti kärpimine
+echo "<br>";
+$tekst2="    Põhitoetus võetakse ära 11.11 kui võlgnevused ei ole parandatud     ";
+echo trim($tekst2, "P, a, k..n, w");
+echo "<br>";
+echo ltrim($tekst2);
+echo "<br>";
+echo rtrim($tekst2);
+echo "<br>";
+//
+
+//Iseseisvalt - tekst kui massiiv
