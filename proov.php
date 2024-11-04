81f577ba683b1674ffde53cd432784b938428324
AleksandrAleksejev <sashaaleksejev813577@gmail.com>
2024-11-04 10:30:15 +0200

    Iseseisvalt - tekst kui massiiv

--- proov.php
+++ proov.php
@@ -43,7 +43,7 @@ echo "<br>";
 echo "sõnade arv laused - ".str_word_count($tekst);//sõnade arv laused
 //Iseseisvalt - teksti kärpimine
 echo "<br>";
-$tekst2="    Põhitoetus võetakse ära 11.11 kui võlgnevused ei ole parandatud     ";
+$tekst2="    Pohitoetus voetakse ara 11.11 kui volgnevused ei ole parandatud     ";
 echo trim($tekst2, "P, a, k..n, w");
 echo "<br>";
 echo ltrim($tekst2);
@@ -53,3 +53,21 @@ echo "<br>";
 //
 
 //Iseseisvalt - tekst kui massiiv
+echo $tekst2[0];
+echo '<br>';
+echo $tekst2[4];
+//
+echo substr($tekst2, 3, 5);
+echo '<br>';
+echo substr($tekst2, 4, -13);
+echo '<br>';
+echo substr($tekst2, -8, 7);
+//
+print_r(str_word_count($tekst2, 1));
+echo '<br>';
+//
+$sona = str_word_count($tekst2, 1);
+echo $sona[2];
+echo '<br>';
+//
+print_r(str_word_count($tekst2, 2));
